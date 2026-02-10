7a949ed3b2464079bdba969d4d8098a8d1c5aac3
Mithilesh Yadav <mithileshyadav012@gmail.com>
2026-02-10 12:59:23 +0530

    Prototype Design Pattern class

---
+++ PrototypeDesignPattern/PrototypeApp.java
@@ -0,0 +1,21 @@
+package PrototypeDesignPattern;
+
+import java.util.List;
+
+public class PrototypeApp {
+    public static void main(String[] args) throws CloneNotSupportedException{
+        Employees emps = new Employees();
+        //time consuming method called one time only
+        emps.loadData();
+
+        //create new emps object using clone
+        Employees empNew = (Employees) emps.clone();
+        //update list in second object
+        List<String> empList = empNew.getEmpList();
+        empList.add("John");
+
+        //print both object list
+        System.out.println(emps.getEmpList());
+        System.out.println(empNew.getEmpList());
+    }
+}

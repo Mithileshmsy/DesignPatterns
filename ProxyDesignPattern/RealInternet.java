72bf96e7f08f1f3dda38ebcb2539ea6ebde3c1b3
Mithilesh Yadav <mithileshyadav012@gmail.com>
2026-02-10 14:13:44 +0530

    Proxy Design Pattern class

---
+++ ProxyDesignPattern/RealInternet.java
@@ -0,0 +1,8 @@
+package ProxyDesignPattern;
+
+public class RealInternet implements Internet{
+    @Override
+    public void connectTo(String host) {
+        System.out.println("Connecting to "+host);
+    }
+}
